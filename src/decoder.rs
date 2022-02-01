f32e8724ee42d85c9820b31ebe05d1554f2eda47
agatan <agatan039@gmail.com>
2022-02-02 00:12:42 +0900

    Refactor

--- src/decoder.rs
+++ src/decoder.rs
@@ -1,22 +1,22 @@
 use std::cmp::Ordering;
 
-use crate::lm::{LMStateRef, LM};
+use crate::lm::{LMStateRef, LM, LMState};
 
 #[derive(Clone, Debug, PartialEq)]
-struct DecoderState {
+struct DecoderState<State: LMState> {
     score: f32,
     token: i32,
     prev_blank: bool,
     am_score: f32,
     lm_score: f32,
     parent_index: isize,
-    lm_state: LMStateRef,
+    lm_state: State,
 }
 
-impl DecoderState {
+impl <State : LMState> DecoderState<State> {
     /// Compare two states by their internal conditions ignoring the scores.
     /// This is used to sort states so that the same states will be consecutive.
-    fn cmp_without_score(&self, other: &DecoderState) -> Ordering {
+    fn cmp_without_score(&self, other: &DecoderState<State>) -> Ordering {
         let lm_cmp = self.lm_state.partial_cmp(&other.lm_state).unwrap();
         if lm_cmp != Ordering::Equal {
             return lm_cmp;
@@ -30,7 +30,7 @@ impl DecoderState {
         }
     }
 
-    fn cmp_without_score_then_score(&self, other: &DecoderState) -> Ordering {
+    fn cmp_without_score_then_score(&self, other: &DecoderState<State>) -> Ordering {
         let without_score = self.cmp_without_score(other);
         if without_score != Ordering::Equal {
             without_score
@@ -39,7 +39,7 @@ impl DecoderState {
         }
     }
 
-    fn cmp_by_score(&self, other: &DecoderState) -> Ordering {
+    fn cmp_by_score(&self, other: &DecoderState<State>) -> Ordering {
         self.score.partial_cmp(&other.score).unwrap()
     }
 }
@@ -89,13 +89,13 @@ pub struct DecoderOptions {
 pub struct Decoder<T: LM> {
     options: DecoderOptions,
     /// All the new candidates that proposed based on the previous step.
-    current_candidates: Vec<DecoderState>,
+    current_candidates: Vec<DecoderState<T::State>>,
     current_best_score: f32,
     current_candidate_pointers: Vec<usize>,
     /// blank_index is the index of the blank token.
     blank: i32,
     /// hypothesis for each time step.
-    hypothesis: Vec<Vec<DecoderState>>,
+    hypothesis: Vec<Vec<DecoderState<T::State>>>,
     /// The language model.
     lm: T,
 }
@@ -342,11 +342,11 @@ impl<T: LM> Decoder<T> {
     }
 }
 
-fn add_candidate(
-    output: &mut Vec<DecoderState>,
+fn add_candidate<T: LMState>(
+    output: &mut Vec<DecoderState<T>>,
     current_best_score: &mut f32,
     beam_threshold: f32,
-    state: DecoderState,
+    state: DecoderState<T>,
 ) {
     if state.score > *current_best_score {
         *current_best_score = state.score;
--- src/lm.rs
+++ src/lm.rs
@@ -1,44 +1,58 @@
+pub mod kenlm;
+
 use std::{cell::RefCell, collections::HashMap, fmt::Debug, rc::Rc};
 
+pub trait LMState: PartialOrd + PartialEq + Clone + Sized + 'static {
+    /// Create a LMStateRef with a given LMState and a token.
+    /// If the LMStateRef already has a child state with the same token, the child state will be returned.
+    /// If not, a new child state will be created and returned.
+    fn child(&self, token: i32) -> Self;
+}
+
 #[derive(Debug, Default)]
-struct LMState {
-    children: HashMap<i32, LMStateRef>,
+pub struct DefaultLMState {
+    children: HashMap<i32, LMStateRef<DefaultLMState>>,
 }
 
 /// A reference to a LM state.
 /// LMStateRef holds the information of the token sequence being decoded, and the identity of the token sequence can be confirmed by comparing LMStateRef.
-#[derive(Clone, Default)]
-pub struct LMStateRef(Rc<RefCell<LMState>>);
+#[derive(Default)]
+pub struct LMStateRef<T>(Rc<RefCell<T>>);
 
-impl LMStateRef {
+impl<T: Default> LMStateRef<T> {
     /// Create a root LMStateRef.
     pub fn new() -> Self {
         Self::default()
     }
+}
 
-    /// Create a LMStateRef with a given LMState and a token.
-    /// If the LMStateRef already has a child state with the same token, the child state will be returned.
-    /// If not, a new child state will be created and returned.
-    pub fn child(&self, token: i32) -> LMStateRef {
+impl <T> Clone for LMStateRef<T> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl LMState for LMStateRef<DefaultLMState> {
+    fn child(&self, token: i32) -> Self {
         let mut state = self.0.borrow_mut();
         let child = state.children.entry(token).or_insert_with(LMStateRef::new);
         child.clone()
     }
 }
 
-impl Debug for LMStateRef {
+impl<T> Debug for LMStateRef<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:p}", self.0.as_ptr())
     }
 }
 
-impl PartialEq for LMStateRef {
+impl<T> PartialEq for LMStateRef<T> {
     fn eq(&self, other: &Self) -> bool {
         self.0.as_ptr() == other.0.as_ptr()
     }
 }
 
-impl PartialOrd for LMStateRef {
+impl<T> PartialOrd for LMStateRef<T> {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         self.0.as_ptr().partial_cmp(&other.0.as_ptr())
     }
@@ -46,12 +60,13 @@ impl PartialOrd for LMStateRef {
 
 /// LM is a wrapper of a language model for decoding.
 pub trait LM {
+    type State: LMState;
     /// Initializes the LM, then returns the root LMStateRef.
-    fn start(&mut self) -> LMStateRef;
+    fn start(&mut self) -> Self::State;
     // Returns the new state and the score when a token comes as a continuation of LMStateRef.
-    fn score(&self, state: &LMStateRef, token: i32) -> (LMStateRef, f32);
+    fn score(&self, state: &Self::State, token: i32) -> (Self::State, f32);
     // Returns the new state and the score of the final state.
-    fn finish(&self, state: &LMStateRef) -> (LMStateRef, f32);
+    fn finish(&self, state: &Self::State) -> (Self::State, f32);
 }
 
 /// ZeroLM is a language model that always returns 0.
@@ -60,15 +75,17 @@ pub trait LM {
 pub struct ZeroLM;
 
 impl LM for ZeroLM {
-    fn start(&mut self) -> LMStateRef {
+    type State = LMStateRef<DefaultLMState>;
+
+    fn start(&mut self) -> LMStateRef<DefaultLMState> {
         LMStateRef::new()
     }
 
-    fn score(&self, state: &LMStateRef, token: i32) -> (LMStateRef, f32) {
+    fn score(&self, state: &LMStateRef<DefaultLMState>, token: i32) -> (LMStateRef<DefaultLMState>, f32) {
         (state.child(token), 0.0)
     }
 
-    fn finish(&self, state: &LMStateRef) -> (LMStateRef, f32) {
+    fn finish(&self, state: &LMStateRef<DefaultLMState>) -> (LMStateRef<DefaultLMState>, f32) {
         (state.clone(), 0.0)
     }
 }

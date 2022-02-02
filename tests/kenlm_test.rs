ef766c78ae799770a279b56c9218a79f485702d3
agatan <agatan039@gmail.com>
2022-02-02 18:28:16 +0900

    Fix version

--- tests/kenlm_test.rs
+++ tests/kenlm_test.rs
@@ -9,18 +9,3 @@ fn kenlm_model_works() {
     let (_, score) = kenlm.finish(&next_state);
     assert_eq!(score, -2.348754);
 }
-
-#[test]
-fn decoder_kenlm_integration_works() {
-    let mut decoder = Decoder::new(
-        DecoderOptions {
-            beam_size: 200,
-            beam_size_token: 200,
-            beam_threshold: f32::MAX,
-            lm_weight: 0.5,
-        },
-        1,
-        KenLM::new("data/lm.arpa"),
-    );
-    decoder.decode()
-}
